c53705000901e8bad389dee422744ece5aeca3cc
Patrick Stein <Patrick.Stein@jinx.eu>
2021-09-01 18:33:40 +0200

    added test for decoding data object.

--- Tests/sma2mqttTests/sma2mqttTests.swift
+++ Tests/sma2mqttTests/sma2mqttTests.swift
@@ -19,17 +19,17 @@ func hex(from string: String) -> Data
 
 final class sma2mqttTests: XCTestCase
 {
-    func testExample() throws
+    func testSMADecoding() throws
     {
         JLog.loglevel = .trace
         
         let data  = hex(from:"534d4100 0004 02a0 00000001 0046 0010 6065 11 e0 07050102030400a19901f6 a22fb3 0001 0000 0000f1b10102005400000000010000000101260068d50f613b975300000000000122260068d50f61b81f00000000000000000000")
+
         let binaryDecoder = BinaryDecoder(data: [UInt8](data) )
-
-        
         let packet = try? SMAMulticastPacket(fromBinary:binaryDecoder)
-
         JLog.debug("Packet:\(packet)")
+        let packet2 = try? SMAMulticastPacket(data:data)
+        JLog.debug("Packet2:\(packet2)")
 
         XCTAssert(true)
     }
