f84641f48bdc2c75ffd083cd8a6ba35138f14462
Patrick Stein <patrick.stein@jinx.de>
2023-07-19 18:20:34 +0200

    fixed docker creation as well as build.sh for raspberry use

--- build.sh
+++ build.sh
@@ -1,70 +1,33 @@
 #!/bin/bash
+#
+# This script downloads the current docker container
+# and starts it with autorestart.
+# I use this script on a raspberry pi running ubuntu
+#
+
+INVERTER_PASSWORD="MyPassword"
+
 
 if [[ $EUID -eq 0 ]]; then
    echo "This script must NOT run as root"
    exit 1
 fi
 
-DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
-
-echo "DIR:$DIR"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 
-packagedir="$DIR"
-buildpath=.build
 programname=sma2mqtt
-releasepath=release
-if [[ ! -d "$packagedir" ]];
-then
-        echo "$packagedir does not exist."
-        exit 1
-fi
-
-cd "$packagedir"
-mkdir "$buildpath"    2>/dev/null
-
-cat >"buildandstart.sh" <<EOF
-#!/bin/bash
-
-executable="$buildpath/$releasepath/$programname"
-if [[ ! -x "\$executable" ]];
-then
-        echo "Did not find executable \$executable - building"
-        swift build -c release --build-path="$buildpath"
-fi
-echo "Executing \$executable"
-exec "\$executable" --interval 0
-EOF
-
-chmod ugo+x "buildandstart.sh"
-
-DOCKER_UID=$(id -u ${USER})
-DOCKER_GID=$(id -g ${USER})
-
-docker pull swiftlang/swift:nightly-5.6-focal
-docker build -t swiftlang/swift:nightly-5.6-focal  -<<EOF
-FROM swiftlang/swift:nightly-5.6-focal
-
-RUN groupadd -g $DOCKER_GID swift
-RUN useradd -m -u $DOCKER_UID -g swift swift
-USER swift
-
-WORKDIR /home/swift
-CMD ["/home/swift/buildandstart.sh"]
-EOF
-
-# docker image inspect swift:latest >/dev/null 2>/dev/null  || docker build -t swift:latest .
-
 docker stop $programname
 docker container rm $programname
 
+version="jollyjinx/$programname:latest"
+#version="jollyjinx/$programname:development"
+
 docker run \
         --detach --restart=always \
-        -u $DOCKER_UID:$DOCKER_GID \
         --net service16 \
         --log-opt max-size=1m --log-opt max-file=2 \
-        -v "$packagedir":/home/swift \
         --name "$programname" \
-        swiftlang/swift:nightly-5.6-focal
+        "$version" "$programname" --inverter-password "$INVERTER_PASSWORD"
 
-docker network connect mqtt-net "$programname"
+#docker network connect mqtt-net "$programname"
--- sma2mqtt.product.dockerfile
+++ sma2mqtt.product.dockerfile
@@ -10,10 +10,14 @@ COPY --from=builder /root/.build/release/sma2mqtt .
 COPY --from=builder /root/.build/release/sma2mqtt_sma2mqttLibrary.resources ./sma2mqtt_sma2mqttLibrary.resources
 CMD ["./sma2mqtt"]
 
-#docker build . --file sma2mqtt.product.dockerfile --tag jollyjinx/sma2mqtt:latest
-#docker push jollyjinx/sma2mqtt:latest
-#docker build . --file sma2mqtt.product.dockerfile --tag sma2mqtt
-#docker run --name sma2mqtt sma2mqtt
-#docker build . --file sma2mqtt.product.dockerfile --tag jollyjinx/sma2mqtt:development
-#docker push jollyjinx/sma2mqtt:development
+# create your own docker image:
+#
+# docker build . --file sma2mqtt.product.dockerfile --tag sma2mqtt
+# docker run --name sma2mqtt sma2mqtt
+
+
+# following lines are for publishing on docker hub
+#
+# docker build . --file sma2mqtt.product.dockerfile --tag jollyjinx/sma2mqtt:latest && docker push jollyjinx/sma2mqtt:latest
+# docker build . --file sma2mqtt.product.dockerfile --tag jollyjinx/sma2mqtt:development && docker push jollyjinx/sma2mqtt:development
 
