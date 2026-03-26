8a8cae90fa672dd212124bd25ac9fac624cb507d
Patrick Stein <patrick.stein@jinx.de>
2026-03-26 17:36:36 +0100

    Bump BinaryCoder dependency to 2.4.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(name: "sma2mqtt",
                           .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.2.2")),
                           .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.18.0"),
                           .package(url: "https://github.com/swift-server-community/mqtt-nio", .upToNextMajor(from: "2.8.0")),
-                          .package(url: "https://github.com/jollyjinx/BinaryCoder", .upToNextMajor(from: "2.3.1")),
+                          .package(url: "https://github.com/jollyjinx/BinaryCoder", .upToNextMajor(from: "2.4.0")),
                           .package(url: "https://github.com/jollyjinx/JLog", .upToNextMajor(from: "0.0.7")),
                       ],
                       targets: [
